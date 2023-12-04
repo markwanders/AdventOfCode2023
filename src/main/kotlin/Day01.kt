6a16892c9402f17e93eee5a0989afe3ae87f5121
Mark <mndmwanders@gmail.com>
2023-12-04 16:20:46 +0100

    Day 1 part 2 WIP

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -8,7 +8,18 @@ class Day01 {
         }
 
     fun part2(input: List<String>): Int {
-        return 0
+        //todo: this doesn't handle duplicates...
+        val toSearch = mapOf("one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9")
+        return input.sumOf { line ->
+            val foundNumbers = mutableMapOf<Int, String>().toSortedMap()
+            toSearch.keys.forEach { number ->
+                val at = line.indexOf(number)
+                if (at >= 0) foundNumbers[at] = toSearch[number]!!
+            }
+            line.forEachIndexed { i, it -> if (it.isDigit()) foundNumbers[i] = it.toString() }
+            println(foundNumbers.values)
+            (foundNumbers.values.first() + foundNumbers.values.last()).toInt()
+        }
     }
 
     companion object {
@@ -18,6 +29,7 @@ class Day01 {
         @JvmStatic
         fun main(args: Array<String>) {
             println(day.part1(input))
+            println(day.part2(input))
         }
     }
 }
