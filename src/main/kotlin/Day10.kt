08c4194e6d13c4ad4bb9003a390bdb6706a3af0b
Mark <mndmwanders@gmail.com>
2023-12-10 16:55:55 +0100

    Solved day part 1

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -7,8 +7,40 @@ class Day10 {
         input.forEachIndexed { y, line ->
             line.forEachIndexed { x, c -> grid[Point(x, y)] = c }
         }
-        print(grid)
-        return 0
+        val start = grid.entries.first { it.value == 'S' }.key
+        val path = mutableSetOf(start)
+        var position = start
+        while (true) {
+            val neighbours = neighbours(position, grid)
+            val nextPoint = neighbours.firstOrNull { !path.contains(it.key) } ?: break
+            position = nextPoint.key
+            path.add(position)
+        }
+        return path.size/2
+    }
+
+    private fun neighbours(position: Point, grid: Map<Point, Char>) : List<Map.Entry<Point, Char>> =
+        grid.entries.filter { (key, value) ->
+            ((key.x == position.x && key.y == position.y + 1 && allowedToGoDown(grid[position]!!, value)) ||
+                    (key.x == position.x && key.y == position.y - 1 && allowedToGoUp(grid[position]!!, value)) ||
+                    (key.x == position.x + 1 && key.y == position.y && allowedToGoRight(grid[position]!!, value)) ||
+                    (key.x == position.x - 1 && key.y == position.y && allowedToGoLeft(grid[position]!!, value)))
+    }
+
+    private fun allowedToGoDown(a: Char, b: Char): Boolean {
+        return listOf('|', '7', 'S', 'F').contains(a) && listOf('|', 'J', 'L', 'S').contains(b)
+    }
+
+    private fun allowedToGoUp(a: Char, b: Char): Boolean {
+        return listOf('|', 'J', 'S', 'L').contains(a) && listOf('|', 'F', '7', 'S').contains(b)
+    }
+
+    private fun allowedToGoRight(a: Char, b: Char): Boolean {
+        return listOf('-', 'F', 'S', 'L').contains(a) && listOf('-', 'J', '7', 'S').contains(b)
+    }
+
+    private fun allowedToGoLeft(a: Char, b: Char): Boolean {
+        return listOf('-', 'J', 'S', '7').contains(a) && listOf('-', 'F', 'L', 'S').contains(b)
     }
 
     companion object {
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -18,7 +18,20 @@ class Day10Test {
         |F--J
         LJ...
     """.trimIndent().split("\n")
-
+    private val testInput3 = """
+        -L|F7
+        7S-7|
+        L|7||
+        -L-J|
+        L|-JF
+    """.trimIndent().split("\n")
+    private val testInput4 = """
+        7-F7-
+        .FJ|7
+        SJLL7
+        |F--J
+        LJ.LJ
+    """.trimIndent().split("\n")
     @Test
     fun testCase1() {
         assertEquals(4, day.part1(testInput1))
@@ -27,5 +40,14 @@ class Day10Test {
     @Test
     fun testCase2() {
         assertEquals(8, day.part1(testInput2))
+    }
+    @Test
+    fun testCase3() {
+        assertEquals(4, day.part1(testInput3))
+    }
+
+    @Test
+    fun testCase4() {
+        assertEquals(8, day.part1(testInput4))
     }
 }
