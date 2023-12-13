5695146baaac12ea002a60aa48d7359a880be281
Mark <mndmwanders@gmail.com>
2023-12-13 12:42:47 +0100

    Solved day 13 part 1

---
+++ src/main/kotlin/Day13.kt
@@ -0,0 +1,40 @@
+import java.io.File
+
+class Day13 {
+    fun part1(input: String): Int {
+        val patterns = input.split("\r\n\r\n")
+        return patterns.sumOf { pattern ->
+            val rows = pattern.split("\n").map { it.trim() }
+            val y = findReflection(rows)
+            val columns = rows.first().indices.map { readColumn(rows, it) }
+            val x = findReflection(columns)
+            x + 100 * y
+        }
+    }
+
+    private fun findReflection(input: List<String>) = (1..<input.size).sumOf { i ->
+        val left = input.subList(0, i)
+        val right = input.subList(i, input.size)
+        val max = minOf(left.size, right.size)
+        val trimmedLeft = left.takeLast(max)
+        val trimmedRight = right.take(max).reversed()
+        if (trimmedRight == trimmedLeft) {
+            i
+        } else {
+            0
+        }
+    }
+
+    private fun readColumn(lines: List<String>, number: Int): String =
+        lines.map { line -> line[number] }.joinToString("")
+
+    companion object {
+        private val day = Day13()
+        private val input = File("src/main/resources/day13.txt").readText()
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(day.part1(input))
+        }
+    }
+}
---
+++ src/test/kotlin/Day13Test.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import kotlin.test.Test
+
+class Day13Test {
+    private val day = Day13()
+    private val testInput1 = """
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+    """.trimIndent()
+
+    @Test
+    fun testCase1() {
+        assertEquals(405, day.part1(testInput1))
+    }
+
+}
